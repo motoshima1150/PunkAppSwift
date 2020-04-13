c28c86cb9107ac39d395c0220489e4df20e1ca75
motoshima1150 <motonari.nakashima@gmail.com>
2020-04-13 23:51:31 +0900

    [add] initial commit

---
+++ PankApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PankApp
+//
+//  Created by Motonari Nakashima on 2020/04/13.
+//  Copyright © 2020 motoshima1150. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
